6f77c9d344e6680f321b944cb038f8c8072e0194
Nisal Fernando <159456416+Nisal277@users.noreply.github.com>
2026-08-27 18:34:04 +0530

    handle missing inputs validation

--- rpi/ml/disease_fusion.py
+++ rpi/ml/disease_fusion.py
@@ -5,3 +5,7 @@ LOG = get_logger(__name__)
 
 
 def fuse(vision_data: Dict[str, Any] = None, nlp_data: Dict[str, Any] = None) -> Dict[str, Any]:
+
+    vision_data = vision_data or {}
+    nlp_data = nlp_data or {}
+
